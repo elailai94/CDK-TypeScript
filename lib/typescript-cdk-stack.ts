50dd40b6374621b879f1734efd0950e47d64cc4b
Elisha Lai <elisha.lai@icloud.com>
2022-10-14 20:21:51 -0400

    feat: add custom construct for networking

--- lib/typescript-cdk-stack.ts
+++ lib/typescript-cdk-stack.ts
@@ -1,20 +1,24 @@
 import * as cdk from "aws-cdk-lib";
-
-import { Bucket, BucketEncryption } from "aws-cdk-lib/aws-s3";
+import * as s3 from "aws-cdk-lib/aws-s3";
 
 import { Construct } from "constructs";
+import { Networking } from "./networking";
 
 export class TypescriptCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bucket = new Bucket(this, "DocumentsBucket", {
-      encryption: BucketEncryption.S3_MANAGED
+    const bucket = new s3.Bucket(this, "DocumentsBucket", {
+      encryption: s3.BucketEncryption.S3_MANAGED
     });
 
     new cdk.CfnOutput(this, "DocumentsBucketNameExport", {
       exportName: "DocumentsBucketName",
       value: bucket.bucketName
+    });
+
+    new Networking(this, "NetworkingConstruct", {
+      maxAzs: 2
     });
   }
 }
