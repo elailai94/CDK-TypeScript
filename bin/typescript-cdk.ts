0f64460347c1d21c78e6fe02fab0e4b46a66307e
Elisha Lai <elisha.lai@icloud.com>
2022-10-24 17:27:03 -0400

    chore: update dependencies

--- bin/typescript-cdk.ts
+++ bin/typescript-cdk.ts
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 
-import "source-map-support/register";
-
 import * as cdk from "aws-cdk-lib";
 
 import { TypescriptCdkStack } from "../lib/typescript-cdk-stack";
 
 const app = new cdk.App();
-const stack = new TypescriptCdkStack(app, "TypescriptCdkStack");
+const stack = new TypescriptCdkStack(app, "TypescriptCdkStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
 
 cdk.Tags.of(stack).add("App", "DocumentManagement");
 cdk.Tags.of(stack).add("Environment", "Development");
